965cf8eb90d9d8d378294d61b7be527690775ba8
rodrib <rodribogado50@gmail.com>
2023-07-16 01:56:05 -0300

    kpi3 all

--- pages/06_Kpi.py
+++ pages/06_Kpi.py
@@ -14,6 +14,8 @@ df1_kpi = pd.read_csv("Acceso a Internet fijo por tecnología y provincia.csv")
 if st.checkbox("Mostrar Acceso a Internet fijo por tecnología y provincia"):
     st.dataframe(df1_kpi)
 
+
+st.markdown("### Penetracion de la Fibra Optica")
 ###########
 
 import altair as alt
@@ -57,4 +59,64 @@ st.altair_chart(alt.Chart(data).mark_line().encode(
 #################
 
 #Internet Ingresos
+
+st.markdown("### Ingresos Trimestrales")
+
+df6_kpi = pd.read_csv("Internet_Ingresos.csv")
+
+
+# Paso 1: Calcular la suma de ingresos por trimestre
+ingresos_trimestrales = df6_kpi.groupby(['Año', 'Trimestre'])['Ingresos (miles de pesos)'].sum()
+
+# Paso 2: Crear una tabla con los resultados
+tabla_resultados = pd.DataFrame(ingresos_trimestrales)
+tabla_resultados.columns = ['Ingresos Trimestrales']
+
+# Configurar la aplicación de Streamlit
+st.title("Ingresos Trimestrales")
+st.table(tabla_resultados)
+
+###################
+
+st.markdown("### Variaciones Trimestrales")
+
+df7_kpi = pd.read_csv("Internet_Penetracion.csv")
+
+
+import streamlit as st
+import pandas as pd
+
+# Ordenar el DataFrame por año y trimestre de forma ascendente
+df7_kpi.sort_values(by=['Año', 'Trimestre'], inplace=True)
+
+# Convertir la columna 'Accesos por cada 100 hogares' a tipo numérico
+df7_kpi['Accesos por cada 100 hogares'] = pd.to_numeric(df7_kpi['Accesos por cada 100 hogares'], errors='coerce')
+
+# Calcular la variación trimestral de Accesos por cada 100 hogares
+df7_kpi['Variación Trimestral'] = df7_kpi['Accesos por cada 100 hogares'].pct_change()
+
+# Calcular la variación anual de Accesos por cada 100 hogares
+df7_kpi['Variación Anual'] = df7_kpi.groupby('Año')['Accesos por cada 100 hogares'].pct_change(periods=4)
+
+# Mostrar los resultados en Streamlit
+st.dataframe(df7_kpi[['Año', 'Trimestre', 'Provincia', 'Variación Trimestral', 'Variación Anual']])
+
+
+###############
+
+df10_kpi = pd.read_csv("Denuncias_y_reclamos_202306-1.csv")
+
+# Calcular el total de reclamos por mes
+total_reclamos_por_mes = df10_kpi.groupby('Mes')['Cantidad'].sum()
+
+# Crear el gráfico de barras
+fig, ax = plt.subplots(figsize=(10, 6))
+total_reclamos_por_mes.plot(kind='bar', ax=ax)
+ax.set_xlabel('Mes')
+ax.set_ylabel('Total de Reclamos')
+ax.set_title('Total de Reclamos por Mes')
+ax.grid(True)
+
+# Mostrar el gráfico en Streamlit
+st.pyplot(fig)
 
